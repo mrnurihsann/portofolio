60441e0426f4d3099140dcad01b806be3bda3416
mrnurihsann <muhammadridhwannurihsan@gmail.com>
2024-07-15 16:27:35 +0700

    update certif

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -4,74 +4,56 @@ import { Link } from "react-router-dom";
 import "./style/navBar.css";
 
 const NavBar = (props) => {
-	const { active } = props;
+  const { active } = props;
 
-	return (
-		<React.Fragment>
-			<div className="nav-container">
-				<nav className="navbar">
-					<div className="nav-background">
-						<ul className="nav-list">
-							<li
-								className={
-									active === "home"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/">Home</Link>
-							</li>
-							<li
-								className={
-									active === "about"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/about">About</Link>
-							</li>
-							<li
-								className={
-									active === "skills"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/skills">Skills</Link>
-							</li>
-							<li
-								className={
-									active === "projects"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/projects">Projects</Link>
-							</li>
-							<li
-								className={
-									active === "contact"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/contact">Contact</Link>
-							</li>
-							<li
-								className={
-									active === "certificates"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/certificates">Certificates</Link>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div className="nav-container">
+        <nav className="navbar">
+          <div className="nav-background">
+            <ul className="nav-list">
+              <li
+                className={active === "home" ? "nav-item active" : "nav-item"}
+              >
+                <Link to="/">Home</Link>
+              </li>
+              <li
+                className={active === "about" ? "nav-item active" : "nav-item"}
+              >
+                <Link to="/about">About</Link>
+              </li>
+              <li
+                className={active === "skills" ? "nav-item active" : "nav-item"}
+              >
+                <Link to="/skills">Skills</Link>
+              </li>
+              <li
+                className={
+                  active === "projects" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li
+                className={
+                  active === "contact" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li
+                className={
+                  active === "certificates" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/certificates">Certif</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default NavBar;
