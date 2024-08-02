fb06953ed7c494feb6131e162b32aea05e86b2e6
mrnurihsann <muhammadridhwannurihsan@gmail.com>
2024-08-02 15:29:15 +0700

    update project

--- src/data/user.js
+++ src/data/user.js
@@ -1,172 +1,178 @@
 const INFO = {
-	main: {
-		title: "mrnurihsann Porto",
-		name: "Nur Ihsan.",
-		email: "muhammadridhwannurihsan@gmail.com",
-		logo: "../me.png",
-	},
+  main: {
+    title: "mrnurihsann Porto",
+    name: "Nur Ihsan.",
+    email: "muhammadridhwannurihsan@gmail.com",
+    logo: "../me.png",
+  },
 
-	socials: {
-		github: "https://github.com/mrnurihsann",
-		linkedin: "https://www.linkedin.com/in/muhammad-ridwan-nur-ihsan-032561306/",
-		instagram: "https://instagram.com/mrnurihsann",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
-	},
+  socials: {
+    github: "https://github.com/mrnurihsann",
+    linkedin:
+      "https://www.linkedin.com/in/muhammad-ridwan-nur-ihsan-032561306/",
+    instagram: "https://instagram.com/mrnurihsann",
+    stackoverflow: "https://stackoverflow.com/",
+    facebook: "https://facebook.com/",
+  },
 
-	homepage: {
-		title: "Muhammad Ridwan Nur Ihsan, S1 Informatika.",
-		description:
-			"I am a frontend developer with expertise in JavaScript. I usually use the react.js and vue.js frameworks. I have experience in building interactive and reliable web applications using various frameworks and technologies. I am also a backend using node.js I enjoy solving complex problems and learning new skills. I'm passionate about creating high-quality code that follows best practices and industry standards. I'm always looking for new challenges and opportunities to grow as a developer.",
-	},
+  homepage: {
+    title: "Muhammad Ridwan Nur Ihsan, S1 Informatika.",
+    description:
+      "I am a frontend developer with expertise in JavaScript. I usually use the react.js and vue.js frameworks. I have experience in building interactive and reliable web applications using various frameworks and technologies. I am also a backend using node.js I enjoy solving complex problems and learning new skills. I'm passionate about creating high-quality code that follows best practices and industry standards. I'm always looking for new challenges and opportunities to grow as a developer.",
+  },
 
-	about: {
-		title: "I’m Muhammad Ridwan Nur Ihsan. I live in Yogyakarta, Indonesian where I design the future.",
-		description:
-			"As a Full Stack Developer, I have the ability to develop web applications comprehensively from frontend to backend. I am skilled in using various technologies and programming languages ​​such as HTML, CSS, JavaScript, React, Vue.js, Java. I'm also learning Node.js, PHP, Laravel. On the frontend, I can create attractive and responsive user interfaces with HTML, CSS, and JavaScript, as well as using frameworks and libraries such as React, Angular, or Vue.js. On the back end, I was able to design and implement APIs, manage databases, and handle business logic using technologies such as Node.js, Express.js, and MongoDB or PostgreSQL databases. Additionally, I also have skills in understanding user needs, designing efficient system architectures, and managing the software development cycle from conception to launch. With my experience and expertise, I am ready to take your project from idea to successful product.",
-	},
+  about: {
+    title:
+      "I’m Muhammad Ridwan Nur Ihsan. I live in Yogyakarta, Indonesian where I design the future.",
+    description:
+      "As a Full Stack Developer, I have the ability to develop web applications comprehensively from frontend to backend. I am skilled in using various technologies and programming languages ​​such as HTML, CSS, JavaScript, React, Vue.js, Java. I'm also learning Node.js, PHP, Laravel. On the frontend, I can create attractive and responsive user interfaces with HTML, CSS, and JavaScript, as well as using frameworks and libraries such as React, Angular, or Vue.js. On the back end, I was able to design and implement APIs, manage databases, and handle business logic using technologies such as Node.js, Express.js, and MongoDB or PostgreSQL databases. Additionally, I also have skills in understanding user needs, designing efficient system architectures, and managing the software development cycle from conception to launch. With my experience and expertise, I am ready to take your project from idea to successful product.",
+  },
 
-	projects: [
-		{
-			title: "React",
-			image: "/cinema.png",
-			description:
-				"Create a movie viewing experience",
-			logo: "https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/movie-react.git",
-		},
+  projects: [
+    {
+      title: "React",
+      image: "/cinema.png",
+      description: "Create a movie viewing experience",
+      logo: "https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/movie-react.git",
+    },
 
-		{
-			title: "PHP",
-			image: "/klajatrif.png",
-			description:
-				"Create a website selling second clothes",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/php/php.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/klaja-thrift.git",
-		},
+    {
+      title: "PHP",
+      image: "/klajatrif.png",
+      description: "Create a website selling second clothes",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/php/php.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/klaja-thrift.git",
+    },
 
-		{
-			title: "HTML 5",
-			image: "/perhiasan.png",
-			description:
-				"Create a website for selling jewelry",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/perhiasan.git",
-		},
+    {
+      title: "HTML 5",
+      image: "/perhiasan.png",
+      description: "Create a website for selling jewelry",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/perhiasan.git",
+    },
 
-		{
-			title: "JavaScript",
-			image: "/gema.png",
-			description:
-				"Create a simple project using javascript",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/Gema-45.git",
-		},
+    {
+      title: "JavaScript",
+      image: "/gema.png",
+      description: "Create a simple project using javascript",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/Gema-45.git",
+    },
 
-		{
-			title: "JavaScript",
-			image: "/cinema.png",
-			description:
-				"Create a movie viewing experience",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/movie-react.git",
-		},
+    {
+      title: "JavaScript",
+      image: "/cinema.png",
+      description: "Create a movie viewing experience",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/movie-react.git",
+    },
 
-		{
-			title: "HTML 5",
-			image: "/shoes.png",
-			description:
-				"create a shoe shop website using HTML, CSS, JavaScript",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/shoes-store.git",
-		},
+    {
+      title: "HTML 5",
+      image: "/shoes.png",
+      description: "create a shoe shop website using HTML, CSS, JavaScript",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/shoes-store.git",
+    },
 
-		{
-			title: "Next Js",
-			image: "/cinemanext.png",
-			description:
-				"Create a movie viewing experience",
-			logo: "https://seeklogo.com/images/N/next-js-logo-60FB9B8D2B-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/movie-react.git",
-		},
+    {
+      title: "Next Js",
+      image: "/sunanime.png",
+      description: "Create a website to display anime lists with api",
+      logo: "https://seeklogo.com/images/N/next-js-logo-60FB9B8D2B-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/sun-anime.git",
+    },
 
-		{
-			title: "Vue Js",
-			image: "/shoe.png",
-			description:
-				"create a simple shoe sales website using vue js",
-			logo: "https://seeklogo.com/images/V/vuejs-logo-17D586B587-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/vue-layout.git",
-		},
+    {
+      title: "Vue Js",
+      image: "/shoe.png",
+      description: "create a simple shoe sales website using vue js",
+      logo: "https://seeklogo.com/images/V/vuejs-logo-17D586B587-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/vue-layout.git",
+    },
 
-		{
-			title: "Java",
-			image: "/mk.png",
-			description:
-				"Create a simple course assessment application",
-			logo: "https://seeklogo.com/images/J/java-logo-7833D1D21A-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/PenilaianMatakuliah-java.git",
-		},
+    {
+      title: "Java",
+      image: "/mk.png",
+      description: "Create a simple course assessment application",
+      logo: "https://seeklogo.com/images/J/java-logo-7833D1D21A-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/PenilaianMatakuliah-java.git",
+    },
 
-		{
-			title: "Flutter",
-			image: "/moviemm.png",
-			description:
-				"Create a mobile-based cinema ticket sales application",
-			logo: "https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/movie-app-flutter.git",
-		},
+    {
+      title: "Flutter",
+      image: "/moviemm.png",
+      description: "Create a mobile-based cinema ticket sales application",
+      logo: "https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/movie-app-flutter.git",
+    },
 
-		{
-			title: "CSS 3",
-			image: "/desaww.png",
-			description:
-				"create a website for Wotawati village",
-			logo: "https://seeklogo.com/images/C/css-3-logo-023C1A7171-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/desa-wotawati.git",
-		},
+    {
+      title: "CSS 3",
+      image: "/desaww.png",
+      description: "create a website for Wotawati village",
+      logo: "https://seeklogo.com/images/C/css-3-logo-023C1A7171-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/desa-wotawati.git",
+    },
 
-		{
-			title: "Bootstrap",
-			image: "/klajamall.png",
-			description:
-				"Create website-based online shopping",
-			logo: "https://seeklogo.com/images/B/bootstrap-logo-3C30FB2A16-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/klaja-mall.git",
-		},
+    {
+      title: "Bootstrap",
+      image: "/klajamall.png",
+      description: "Create website-based online shopping",
+      logo: "https://seeklogo.com/images/B/bootstrap-logo-3C30FB2A16-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/klaja-mall.git",
+    },
 
-		{
-			title: "Tailwind",
-			image: "/shoe.png",
-			description:
-				"create a simple shoe sales website using taildwind",
-			logo: "https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann",
-		},
+    {
+      title: "Tailwind",
+      image: "/shoe.png",
+      description: "create a simple shoe sales website using taildwind",
+      logo: "https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann",
+    },
 
-		{
-			title: "Dart",
-			image: "/dart.png",
-			description:
-				"Create a display like a simple location",
-			logo: "https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png",
-			linkText: "View Project",
-			link: "https://github.com/mrnurihsann/LayoutFlutterApp.git",
-		},
-	],
+    {
+      title: "Dart",
+      image: "/dart.png",
+      description: "Create a display like a simple location",
+      logo: "https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/LayoutFlutterApp.git",
+    },
+
+    {
+      title: "Dart",
+      image: "/petshop.png",
+      description: "Create a random dog image application",
+      logo: "https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/dogs-image-random.git",
+    },
+
+    {
+      title: "JavaScript",
+      image: "/carhatt.png",
+      description: "Create a simple website about Carhatt stores",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com/mrnurihsann/rep-carhatt.git",
+    },
+  ],
 };
 
 export default INFO;
